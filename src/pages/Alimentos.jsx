82e94c217ef39dd270220785a2b95d8a9dfb2acb
JuanGonzalo <juangonzalo210500@gmail.com>
2024-06-30 18:21:06 +0200

    subir frontend

--- src/pages/Alimentos.jsx
+++ src/pages/Alimentos.jsx
@@ -1,7 +1,7 @@
 // Importaciones de pages/Alimentos.jsx
 import TablaAlimentos from '../components/Alimentos_components/TablaAlimentos.jsx'
 import FilterAlimentos from '../components/Alimentos_components/FilterAlimentos.jsx'
-import NewAlimentoButton from '../components/Alimentos_components/newAlimentoButton.jsx'
+import NewAlimentoButton from '../components/Alimentos_components/NewAlimentoButton.jsx'
 import '../components/ButtonsToCreate.scss'
 
 //* Página de Alimentos
